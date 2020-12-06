1bc83654cea2f6716d3cb9df2d390b89656b0ce8
andrianitan <andrianitan.84@gmail.com>
2020-12-07 06:13:50 +0700

    Add files via upload

---
+++ build/precache-manifest.4db5e241e73cdb881ab30ccf9f82b3c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "813026e051f40a2c4b29f6b9aa9a4441",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ccb8e5cb2d8b4ed6314",
+    "url": "/static/css/2.1afe660f.chunk.css"
+  },
+  {
+    "revision": "8b72050b3faf76da33a1",
+    "url": "/static/css/main.032932a3.chunk.css"
+  },
+  {
+    "revision": "2ccb8e5cb2d8b4ed6314",
+    "url": "/static/js/2.62b43d08.chunk.js"
+  },
+  {
+    "revision": "9fda8197ca6e25979aa843f42451bfaf",
+    "url": "/static/js/2.62b43d08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b72050b3faf76da33a1",
+    "url": "/static/js/main.61e0ad5e.chunk.js"
+  },
+  {
+    "revision": "fa534945b5311518bb53",
+    "url": "/static/js/runtime-main.8d2f1a3b.js"
+  }
+]);
